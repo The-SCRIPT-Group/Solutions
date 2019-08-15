a2b419078ed0b993e9ae01e73f785361288acfe1
Aditya <43269300+AdityaDesai-99@users.noreply.github.com>
2019-08-16 00:56:01 +0530

    Check till sqrt

--- RSC-2019/Round-1/1/Solution.c
+++ RSC-2019/Round-1/1/Solution.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int prime(int n) {
     int c, i;
     c = 0;
-    for (i = 2; i < n/2; i++) {
+    for (i = 2; i < sqrt(n); i++) {
         if (n%i == 0) {
             c++;
         }
