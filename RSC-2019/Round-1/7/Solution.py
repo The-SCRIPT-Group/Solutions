64b045c26be1a8f4b68ffefb3fbb7da69edd745a
ksdfg <ksdfg123@gmail.com>
2019-08-22 02:16:46 +0530

    added python solution for gift wrap

---
+++ RSC-2019/Round-1/7/Solution.py
@@ -0,0 +1,22 @@
+from math import ceil
+
+word = input()
+
+wrap = ''
+layer = ''
+
+length = len(word) + (len(word) - 1)
+
+
+for i in word:
+    wrap += layer + (i * length) + layer[::-1] + '\n'
+    layer += i
+    length -= 2
+
+for i in word[::-1]:
+    if length > 0:
+        wrap += layer + (i * length) + layer[::-1] + '\n'
+    layer = layer[::-1].replace(i, '', 1)[::-1]
+    length += 2
+    
+print(wrap)
