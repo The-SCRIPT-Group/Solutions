1e9e006ce99496c60264fa9814567c0cd25f00ea
ksdfg <ksdfg123@gmail.com>
2019-08-25 22:12:53 +0530

    added python solution for split the prize

--- RSC-2019/Round-1/6/Solution.py
+++ RSC-2019/Round-1/6/Solution.py
@@ -1,11 +1,4 @@
-s = input()
-
-res = ''
-
-for i in s:
-    if i == '4':
-        res += '1'
-    else:
-        res += '0'
-
-print(str(int(s.replace('4', '3'))) + '\n' + str(int(res)), end='')
+import re
+n = input()
+print(n.replace('4', '3'))
+print(re.sub('[123567890]', '0', n).replace('4', '1'), end='')
