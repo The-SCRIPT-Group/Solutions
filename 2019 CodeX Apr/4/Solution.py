eaa000a0a977053860f4246ea96998b2340f15da
Aniket Raj <aniketronaldo10@gmail.com>
2019-10-01 00:52:01 +0530

    Updated comment

--- 2019 CodeX Apr/4/Solution.py
+++ 2019 CodeX Apr/4/Solution.py
@@ -1,3 +1,5 @@
 print("MK-"+str(int(input(), 2)))
-#int() function can take 2 arguments - the number/string to convert to int and the base of given number/string.
-#int() function will always convert the number to decimal format if it isn't already in it.
+'''
+int() function can take 2 arguments - the number/string to convert to int and the base of given number/string.
+int() function will always convert the number to decimal format if it isn't already in it.
+'''
