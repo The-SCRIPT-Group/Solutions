37453b5ccafced08afc998f5427a8a28ade50cf2
Aniket Raj <aniketronaldo10@gmail.com>
2019-10-04 17:20:16 +0530

    Added solution for python beginners

--- 2019 CodeX Apr/1/Solution.py
+++ 2019 CodeX Apr/1/Solution.py
@@ -1,2 +1,12 @@
+# For Python Beginners
+p = int(input())  # Number of people
+s = int(input())  # Number of shwarmas
+
+r = p % s         # Remaining shwarmas after even distribution
+
+print(r)
+
+# Optimized Solution
+
 # Accept the input as int, and compute and display the result
 print(int(input()) % int(input()))
